8f52b3e97793b96c4f2d7db7514a4f1f367d3320
Anunay Kumar <annunay711998@gmail.com>
2022-08-24 09:37:58 +0530

    Change to our value controller

--- Interface_assingnment/Program.cs
+++ Interface_assingnment/Program.cs
@@ -12,7 +12,7 @@ namespace Interface_assingnment
         void LeaVeDetails();
         void gratuityAmount();
 
-       }
+    }
 
     interface GovtRules1
     {
@@ -24,7 +24,9 @@ namespace Interface_assingnment
 
     class TCS : GovtRules
     {
-        int empid, basic_salary, service_year, GratuityAmount;
+        int empid;
+        float service_year;
+        double basic_salary, GratuityAmount;
         string name, dept, desg;
         public void EmployeePF()
         {
@@ -37,12 +39,12 @@ namespace Interface_assingnment
             Console.WriteLine("The designation of the employee:");
             desg = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Write the Basic salary:");
-            basic_salary = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The balance of Employee PF account" + 0.24 * basic_salary);
+            basic_salary = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The balance added to the Employee PF account: " + 0.24 * basic_salary);
             Console.ReadLine();
         }
 
-        
+
 
         public void gratuityAmount()
         {
@@ -75,7 +77,7 @@ namespace Interface_assingnment
 
         }
 
-       
+
 
         public void LeaVeDetails()
         {
@@ -87,21 +89,96 @@ namespace Interface_assingnment
 
         }
 
-       
-
-          internal class Program
+        class Accenture : GovtRules
+        {
+            int empid;
+            float service_year;
+            double basic_salary, GratuityAmount;
+            string name, dept, desg;
+            public void EmployeePF()
             {
-             static void Main(string[] args )
+                Console.WriteLine("The employee id is:");
+                empid = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The name of employee:");
+                name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("The Departement of the employee:");
+                dept = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("The designation of the employee:");
+                desg = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Write the Basic salary:");
+                basic_salary = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("The balance added to the Employee PF account: " + 0.24 * basic_salary);
+                Console.ReadLine();
+            }
+
+
+
+            public void gratuityAmount()
+            {
+                Console.WriteLine("Write the service year:");
+                service_year = Convert.ToInt32(Console.ReadLine());
+                //basic_salary = Convert.ToInt32(Console.ReadLine());
+                if (service_year > 5 || service_year > 30)
+                {
+                    if (service_year > 10 || service_year <= 19 || service_year >= 6)
+                    {
+                        GratuityAmount = 2 * basic_salary;
+                        Console.WriteLine("The Gratutiy amount is :" + GratuityAmount);
+                        //GratuityAmount = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else if (service_year > 20)
+                    {
+                        GratuityAmount = 3 * basic_salary;
+                        Console.WriteLine("The Gratutiy amount is :" + GratuityAmount);
+                        //GratuityAmount = Convert.ToInt32(Console.ReadLine());
+                    }
+                    else
+                    {
+                        GratuityAmount = 0;
+                        Console.WriteLine("The Gratutiy amount is :" + GratuityAmount);
+                        //GratuityAmount = Convert.ToInt32(Console.ReadLine());
+                    }
+
+                }
+
+
+            }
+
+
+
+            public void LeaVeDetails()
+            {
+                Console.WriteLine("Leave Details for Accenture is ");
+                Console.WriteLine("2 day of Casual Leave per month");
+                Console.WriteLine("5 days of Sick Leave per year");
+                Console.WriteLine("5 days of Previlage Leave per year");
+                Console.WriteLine("Gratuity amount : Not applicable");
+                Console.ReadLine();
+
+            }
+
+
+
+
+            internal class Program
+            {
+                static void Main(string[] args)
                 {
                     TCS pte = new TCS();
                     pte.EmployeePF();
                     pte.gratuityAmount();
                     pte.LeaVeDetails();
-                    
-                    
-                    
+
+                    Accenture ctc = new Accenture();
+                    ctc.EmployeePF();
+                    ctc.gratuityAmount();
+                    ctc.LeaVeDetails();
+
+
+
                 }
 
-             }
+            }
         }
     }
+}
